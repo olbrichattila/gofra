35cb369e028957281a016dd7d6e902d6354a7d69
Attila Olbrich <olbrichattila@gmx.co.uk>
2025-06-08 10:40:26 +0100

    Add entity

---
+++ pkg/app/entity/entity.go
@@ -0,0 +1,240 @@
+package entity
+
+import (
+	"errors"
+	"reflect"
+	"strings"
+
+	"github.com/olbrichattila/gofra/pkg/app/db"
+)
+
+const (
+	tagFieldName = "fieldName"
+	tagTableName = "tableName"
+)
+
+type fieldDef struct {
+	name  string
+	value any
+}
+
+func Nullable[T any](v T) *T {
+	return &v
+}
+
+func ById[T any](db db.DBer, id int64) (T, error) {
+	var entity T
+	_, tableName, _, err := parseEntity(entity)
+	if err != nil {
+		return entity, err
+	}
+
+	row, err := db.QueryOne("SELECT * FROM `"+tableName+"` WHERE `id` = ?", id)
+	if err != nil {
+		return entity, err
+	}
+
+	if err := mapToStruct(row, &entity); err != nil {
+		return entity, err
+	}
+
+	return entity, nil
+}
+
+// All is an alias of ByWhere without parameters
+func All[T any](db db.DBer) ([]T, error) {
+	return ByWhere[T](db, "")
+}
+
+func ByWhere[T any](db db.DBer, where string, params ...any) ([]T, error) {
+	var entity T
+	_, tableName, _, err := parseEntity(entity)
+	if err != nil {
+		return nil, err
+	}
+
+	sql := "SELECT * FROM `" + tableName + "` " + where
+	rows := db.QueryAll(sql, params...)
+	if err := db.GetLastError(); err != nil {
+		return nil, err
+	}
+
+	var result []T
+	for row := range rows {
+		var item T
+		if err := mapToStruct(row, &item); err == nil {
+			result = append(result, item)
+		}
+	}
+	return result, nil
+}
+
+func Delete(db db.DBer, entity any) error {
+	_, tableName, id, err := parseEntity(entity)
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Execute("DELETE FROM `"+tableName+"` WHERE `id` = ?", id)
+	return err
+}
+
+func Save(db db.DBer, entity any) error {
+	fields, tableName, id, err := parseEntity(entity)
+	if err != nil {
+		return err
+	}
+
+	var (
+		sql  string
+		args []any
+	)
+	if id == 0 {
+		sql, args, err = toInsertSQL(fields, tableName)
+	} else {
+		sql, args, err = toUpdateSQL(id, fields, tableName)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Execute(sql, args...)
+	return err
+}
+
+func parseEntity(entity any) ([]fieldDef, string, int64, error) {
+	t := reflect.TypeOf(entity)
+	v := reflect.ValueOf(entity)
+
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+		v = v.Elem()
+	}
+	if t.Kind() != reflect.Struct {
+		return nil, "", 0, errors.New("parseEntity: not a struct or pointer to struct")
+	}
+
+	tableName := strings.ToLower(t.Name())
+	fields := make([]fieldDef, 0, t.NumField())
+	var id int64
+
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		if field.PkgPath != "" {
+			continue // skip unexported
+		}
+
+		if tn := field.Tag.Get(tagTableName); tn != "" {
+			tableName = tn
+			continue
+		}
+
+		name := field.Tag.Get(tagFieldName)
+		if name == "" {
+			name = field.Name
+		}
+
+		val := v.Field(i)
+		if name == "id" && val.Kind() == reflect.Int64 {
+			id = val.Int()
+		}
+
+		if val.Kind() == reflect.Ptr && val.IsNil() {
+			fields = append(fields, fieldDef{name: name})
+			continue
+		}
+
+		if val.Kind() == reflect.Ptr {
+			val = val.Elem()
+		}
+
+		fields = append(fields, fieldDef{name: name, value: val.Interface()})
+	}
+	return fields, tableName, id, nil
+}
+
+func toInsertSQL(fields []fieldDef, table string) (string, []any, error) {
+	var cols []string
+	var placeholders []string
+	var args []any
+
+	for _, f := range fields {
+		if f.name == "id" {
+			continue
+		}
+		cols = append(cols, "`"+f.name+"`")
+		placeholders = append(placeholders, "?")
+		args = append(args, f.value)
+	}
+
+	sql := "INSERT INTO `" + table + "` (" + strings.Join(cols, ",") + ") VALUES (" + strings.Join(placeholders, ",") + ")"
+	return sql, args, nil
+}
+
+func toUpdateSQL(id int64, fields []fieldDef, table string) (string, []any, error) {
+	var sets []string
+	var args []any
+
+	for _, f := range fields {
+		if f.name == "id" {
+			continue
+		}
+		sets = append(sets, "`"+f.name+"`=?")
+		args = append(args, f.value)
+	}
+
+	sql := "UPDATE `" + table + "` SET " + strings.Join(sets, ",") + " WHERE `id` = ?"
+	args = append(args, id)
+	return sql, args, nil
+}
+
+func mapToStruct(data map[string]any, dest any) error {
+	ptrVal := reflect.ValueOf(dest)
+	if ptrVal.Kind() != reflect.Ptr || ptrVal.Elem().Kind() != reflect.Struct {
+		return errors.New("mapToStruct: destination must be pointer to struct")
+	}
+
+	val := ptrVal.Elem()
+	typ := val.Type()
+
+	for i := 0; i < typ.NumField(); i++ {
+		field := typ.Field(i)
+		if field.PkgPath != "" || field.Tag.Get(tagTableName) != "" {
+			continue
+		}
+
+		key := field.Tag.Get(tagFieldName)
+		if key == "" {
+			key = field.Name
+		}
+
+		mapVal, ok := data[key]
+		if !ok || mapVal == nil {
+			continue
+		}
+
+		fieldVal := val.Field(i)
+		mv := reflect.ValueOf(mapVal)
+
+		if fieldVal.Kind() == reflect.Ptr {
+			elem := reflect.New(fieldVal.Type().Elem())
+			if mv.Type().AssignableTo(elem.Elem().Type()) {
+				elem.Elem().Set(mv)
+			} else if mv.Type().ConvertibleTo(elem.Elem().Type()) {
+				elem.Elem().Set(mv.Convert(elem.Elem().Type()))
+			} else {
+				continue
+			}
+			fieldVal.Set(elem)
+		} else {
+			if mv.Type().AssignableTo(fieldVal.Type()) {
+				fieldVal.Set(mv)
+			} else if mv.Type().ConvertibleTo(fieldVal.Type()) {
+				fieldVal.Set(mv.Convert(fieldVal.Type()))
+			}
+		}
+	}
+
+	return nil
+}
